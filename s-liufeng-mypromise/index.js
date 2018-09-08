3fa8a1beff4f2147d1607614953ba74448dff4f3
peakleo <liufeng@lemediaweb.com>
2018-09-08 17:04:58 +0800

    rm  promise

--- s-liufeng-mypromise/index.js
+++
@@ -1,4 +0,0 @@
-const charStat = require('./charStat.js');
-const charStat2 = require('./charStat2.js');
-
-module.exports = {charStat: charStat, charStat2: charStat2};
--- s-liufeng-mypromise/share_20171228/how/h2.js
+++
@@ -1,9 +0,0 @@
-function loadImg() {
-    return new Promise((resolve, reject) => {
-        let URL = "http://cdn103.img.lizhi.fm/audio_cover/2016/12/17/2574145067906931719_320x320.jpg";
-        let img = new Image();
-        img.onload = resolve;
-        img.onerror = reject;
-        img.src = URL;
-    })
-}
